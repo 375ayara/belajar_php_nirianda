5e25e4d1fd19c373d20eb339374b3eae0a4644c6
375ayara <niriandakaylilaar@gmail.com>
2025-02-25 15:26:15 +0700

    [~] edit file

--- skenario ketiga.php
+++ skenario ketiga.php
@@ -22,51 +22,93 @@
 // Sebelum tidur, Andi dan keluarganya memiliki kebiasaan mengobrol bersama keluarga selama 30 menit. Mengobrol bersama keluarga dilakukan setelah tugas sekolah Andi selesai dikerjakan. Dapat dimajukan jika tidak ada tugas sekolah.
 // Andi tidur jam 22:00 dan bangun jam 04:00.
 
-/* Bahan diskusi:
-1. Apa yang perlu diperhatikan saat menentukan jadwal Andi, sehingga tidak ada jadwal yang bertumbuk atau terlewatkan?
-2. Jelaskan alasanmu dalam menentukan urutan kegiatan tersebut!
-3. Jam berapa Andi dan Raya melakukan chatting waktu lokal rumah Raya?
-4. Apakah masih ada waktu untuk Andi memiliki waktu luang? Jam berapakah?
-5. Jika Andi tidak memiliki tugas sekolah, berapakah waktu luang yang Andi miliki?
-*/ 
+//Menampilkan jadwal keseharian Andi 
+echo "<h2>Jadwal Harian</h2>";
+echo "<ul>";
+echo "<li><strong>15:30-15:45</strong>: Andi pulang sekolah</li>";
+echo "<li><strong>15:45-16:10</strong>: Andi sedang mandi</li>";
+echo "<li><strong>16:10-16:40</strong>: Andi sedang mengaji</li>";
+echo "<li><strong>16:40-17:10</strong>: Andi sedang menghafalkan festival kesenian budaya</li>";
+echo "<li><strong>17:10-17:40</strong>: Andi sedang membeli bumbu</li>";
+echo "<li><strong>17:40-18:10</strong>: Andi sedang chattingan</li>";
+echo "<li><strong>18:10-18:35</strong>: Andi sedang sholat maghrib</li>";
+echo "<li><strong>18:35-19:05</strong>: Andi sedang makan malam/li>";
+echo "<li><strong>19:05-19:30</strong>: Andi sedang sholat isya</li>";
 
-$jam = "22:00";
+if ($tugas == "ada")  {
+    echo "<li><strong>19:30-21:30</strong>: Andi sedang mengerjakan tugas</li>";
+    echo "<li><strong>21:30-22:00</strong>: Andi sedang mengobrol dengan keluarga</li>";
+} else {
+    if ($waktu_ngobrol == "maju") {
+        echo "<li><strong>19:30-20:00</strong>: Andi sedang mengobrol dengan keluarga</li>";
+        echo "<li><strong>20:00-22:00</strong>: Waktu luang</li>";
+    } else {
+        echo "<li><strong>19:30-21:30</strong>: Waktu luang</li>";
+        echo "<li><strong>21:30-22:00</strong>: Ngobrol dengan keluarga</li>";
+    }
+}
 
-if($jam == "15:30") {
+echo "<li><strong>22:00-23:59</strong>: Andi sedang tidur</li>";
+echo "<li><strong>00:00-04:00</strong>: Andi sedang tidur</li>";
+echo "<li><strong>04:00-06:45</strong>: Andi sedang siap-siap sekolah</li>";
+echo "<li><strong>06:45-15:30</strong>: Andi sedang sekolah</li>";
+
+$jam = "19:35"; 
+$tugas = "tidak ada"; 
+$waktu_ngobrol = "maju";
+
+if ($jam == "15:30") {
     echo "<br/> $jam Pulang Sekolah"; 
-} else if($jam == "15:45") {
+} else if ($jam == "15:45") {
     echo "<br/> $jam Sampai di Rumah";
-} else if($jam >=date("15:45") && $jam < "16:10") {
+} else if ($jam >= "15:45" && $jam < "16:10") {
     echo "<br/> $jam Mandi"; 
-} else if($jam >= "16:10" && $jam < "16:40") {
+} else if ($jam >= "16:10" && $jam < "16:40") {
     echo "<br/> $jam Mengaji"; 
-} else if($jam >= "16:40" && $jam < "17:10") {
-    echo "<br/> Menghafalkan dialog festival kesenian budaya";
-} else if($jam >= "17:10" && $jam < "17:40") {
+} else if ($jam >= "16:40" && $jam < "17:10") {
+    echo "<br/> $jam Menghafalkan dialog festival kesenian budaya";
+} else if ($jam >= "17:10" && $jam < "17:40") {
     echo "<br/> $jam Beli bumbu";
-} else if($jam >= "17:40" && $jam < "18:10") {
+} else if ($jam >= "17:40" && $jam < "18:10") {
     echo "<br/> $jam Chattingan";
-} else if($jam >= "18:10" && $jam < "18:35") {
-    echo "<br/> $jam Sholat Magrib";
-} else if($jam >= "18:35" && $jam < "19:05") {
+} else if ($jam >= "18:10" && $jam < "18:35") {
+    echo "<br/> $jam Sholat Maghrib";
+} else if ($jam >= "18:35" && $jam < "19:05") {
     echo "<br/> $jam Makan malam";
-} else if($jam >= "19:05" && $jam < "19:30") {
+} else if ($jam >= "19:05" && $jam < "19:30") {
     echo "<br/> $jam Sholat Isya";
-} else if($jam >= "19:30" && $jam < "21:30") {
-    echo "<br/> $jam Mengerjakan tugas";
-} else if($jam >= "21:30" && $jam < "22:00") {
-    echo "<br/> $jam Ngobrol dengan keluarga";
-} else if ($jam == date("04:00")) {
-    echo "<br/> $jam Bangun Tidur";
-} else if($jam >= "22:00" || $jam < "04:00") {
+} else if ($jam >= "19:30" && $jam < "22:00") {
+    if ($tugas == "ada") {
+        if ($jam >= "19:30" && $jam < "21:30") {
+            echo "<br/> $jam Mengerjakan tugas";
+        } else if ($jam >= "21:30" && $jam < "22:00") {
+            echo "<br/> $jam Ngobrol dengan keluarga";
+        }
+    } else {
+        if ($waktu_ngobrol == "maju") {
+            if ($jam >= "19:30" && $jam < "20:00") {
+                echo "<br/> $jam Ngobrol dengan keluarga";
+            } else {
+                echo "<br/> $jam Waktu luang";
+            }
+        } else {
+            if ($jam >= "21:30" && $jam < "22:00") {
+                echo "<br/> $jam Ngobrol dengan keluarga";
+            } else {
+                echo "<br/> $jam Waktu luang";
+            }
+        }
+    }
+} else if ($jam == "22:00" || $jam < "04:00") {
     echo "<br/> $jam Tidur";
+} else if ($jam == "04:00") {
+    echo "<br/> $jam Bangun Tidur";
+} else if ($jam >= "04:00" && $jam <= "06:45") {
+    echo "<br/> $jam Siap-siap sekolah";
+} else if ($jam >= "06:45" && $jam <= "15:30") {
+    echo "<br/> $jam Sekolah";
 }
-else
-{
-    echo "dunia mimpi";
-}
-
-?> 
+    ?>
 
 <?php
 echo "<p>1. Apa yang perlu diperhatikan saat menentukan jadwal Andi, sehingga tidak ada jadwal yang bertumbuk atau terlewatkan? </br> => Agar jadwal Andi tidak bertabrakan/terlewatkan, maka kita memastikan semua kegiatan Andi memiliki rentang waktu yang jelas.</br></p>";
